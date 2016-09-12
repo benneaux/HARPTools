86145848107eeca6cfb430fc146979808ecd483f
benneaux <benjamin.g.moran@gmail.com>
2016-09-12 12:39:48 +1000

    Making changes to reflect new report type.

---  cleanup.R
+++  cleanup.R
@@ -7,7 +7,7 @@ require(stringr)
 
 ## Import ====================================
 
-file.name <- "Dispensing_HCV July 2016.XLS" # name of the file in quotes
+file.name <- "HBV July 2015_June 2016.XLS" # name of the file in quotes
 
 import.data <- 
   as.data.table(
@@ -24,31 +24,69 @@ import.data <-
   # junk rows there are a the start and 
   # end of the dataset.
 
-    junk.s <- 7 # the number at the start
-    junk.e <- 2 # the number at the end
+  # Create clean.data by removing blank rows.
+    clean.data <- import.data[
+      rowSums(is.na(import.data))# Calculates no. of NAs in each row 
+      != ncol(import.data),] # Calculates number of columns present.
 
-      clean.data <- import.data %>%
-        na.omit(.[,1]) %>%
-        slice(junk.s:(n()-junk.e))
 
-      temp <- clean.data %>%
-        subset(select = -1) %>%
-        filter(is.na(.[,1])== FALSE)
-
-      temp.a <- temp[ c(TRUE,FALSE), ] %>% subset(select = -3)
-      temp.b <- temp[ !c(TRUE,FALSE), ] %>% subset(select = -2)
-      data.b <- as.data.frame(cbind(temp.a,temp.b))
-      names(data.b) <- c("Units","Date","Cost","Code")
+  junk.s <- 7 # the number at the start
+  junk.e <- 2 # the number at the end     
+      
+  clean.data <- clean.data %>%
+    slice(junk.s:(n()-junk.e))
   
-      rm(temp, temp.a, temp.b)
+  # Removes "Dispensing Style: " rows from dataset.
+  clean.data <- clean.data[!str_detect(clean.data[,1],"Dispensing Style: "),]
+  
+  # Separate out data fields from all cols except 1.    
+    temp <- clean.data %>%
+      subset(select = -1)
+    
+    temp <- temp[
+      rowSums(is.na(temp)) 
+      != ncol(temp),]
+    
+    # Removes both "Dispensed by: " and "Number of Items Dispensed".
+    temp<- temp[!str_detect(temp[,1],"Dispensed") | is.na(temp[,1]), ]
+    # Removes empty columns
+    temp<- temp[,colSums(is.na(temp)) != nrow(temp)]
+    
+    # Returns odd-numbered rows
+    temp.a <- temp[ c(TRUE, FALSE), ]
+    # Removes empty columns
+    temp.a <- temp.a[,colSums(is.na(temp.a)) != nrow(temp.a)]
+    # Returns even-numbered rows
+    temp.b <- temp[ !c(TRUE,FALSE), ]
+    # Removes empty columns
+    temp.b <- temp.b[,colSums(is.na(temp.b)) != nrow(temp.b)]
+    # Combines each odd-even pair into a single record.
+    data.b <- as.data.frame(cbind(temp.a,temp.b))
+    # Names columns
+    names(data.b) <- c("Prescriber","Units","Date","Cost","Code")
+    
+    # Removes unnescessary text from "Prescriber:"
+    data.b[,"Prescriber"] <- str_replace(
+      data.b[,"Prescriber"],
+      "Prescriber: ",
+      ""
+      )
+    # Removes unnescessary text from "Date:"
+    data.b[,"Date"] <- str_replace(
+      data.b[,"Date"],
+      "Date: ",
+      ""
+    )
+
+  # Clear temporary variables.
+  rm(temp, temp.a, temp.b)
 
 ## Filter Sites =====================
   
   data <- clean.data 
   
   temp <- data %>%
-    subset(select = 1) %>%
-    filter(is.na(.[,1])== FALSE)
+    subset(select = 1)
   
     
   temp.ind <- which(
@@ -74,7 +112,7 @@ import.data <-
       temp.ind.diff[length(temp.ind.diff)+1] <- nrow(temp)-temp.ind[length(temp.ind)- 1] + 1  
     }
   
-  clean.data <- cbind(
+  clean.data2 <- cbind(
     temp,
     rep(
       temp.val$X1,
@@ -82,16 +120,14 @@ import.data <-
       )
     )
   
-  clean.data <- clean.data[-c(temp.ind),]
+  clean.data2 <- clean.data2[-c(temp.ind),]
 
   rm(data, temp, temp.val, temp.ind, temp.ind.diff)
   
 ## Filter Cost Centres ================================= 
 
   # Use the output from the previous section.
-
-  
-  data <- clean.data
+  data <- clean.data2
     
   temp <- data %>%
     subset(select = 1)
@@ -118,22 +154,25 @@ import.data <-
   if(nrow(temp)!=temp.ind[length(temp.ind)]){
     temp.ind.diff[(length(temp.ind.diff)+1)] <- nrow(temp)-temp.ind[length(temp.ind)] + 1
   } else {
-    temp.ind.diff[(length(temp.ind.diff)+1)] <- nrow(temp)- temp.ind[(length(temp.ind)-1)]  
+    temp.ind.diff[(length(temp.ind.diff)+1)] <- nrow(temp)- temp.ind[(length(temp.ind)-1)] + 1  
   }
   
-  clean.data <- cbind(
+  clean.data3 <- cbind(
     data,
     rep(
       temp.val$X1,
       temp.ind.diff
     )
   )
-  clean.data <- clean.data[-c(temp.ind),]
+  clean.data3 <- clean.data3[-c(temp.ind),]
   
   rm(data, temp, temp.val, temp.ind, temp.ind.diff)
-## Split the first column into MRNs and Drugs ============
 
-  data <- clean.data
+## Up to here!!!!!!!! ============
+
+# NOTE: NEED TO CLEAR OUT INPATIENT, OUTPATIENT & DISCHARGE FROM COL.
+break() 
+  data <- clean.data3
   
   temp <- data %>%
     subset(select = 1)
