bbfc7c98cda3c8c959e3f7daebcf9cde6e0b960a
benneaux <benjamin.g.moran@gmail.com>
2016-09-13 17:14:29 +1000

    Cleanup.R now cleans HBV Disp data.

---  cleanup.R
+++  cleanup.R
@@ -1,9 +1,15 @@
-require(dplyr)
-require(tidyr)
-require(data.table)
-require(readxl)
-require(reshape2)
-require(stringr)
+# Cleans up HBV Dispensing Report.
+
+## Setup =======================================================================
+
+req.packages <- c("dplyr","tidyr","data.table","readxl","reshape2","stringr","lubridate")
+
+lapply(
+  req.packages,
+  require,
+  character.only = TRUE,
+  quietly = TRUE
+  )
 
 ## Import ====================================
 
@@ -77,41 +83,54 @@ import.data <-
       "Date: ",
       ""
     )
-
+    
+  data <- clean.data
   # Clear temporary variables.
-  rm(temp, temp.a, temp.b)
+  rm(clean.data,temp, temp.a, temp.b, junk.e, junk.s, file.name)
 
 ## Filter Sites =====================
   
-  data <- clean.data 
-  
+  # Select the first column from our data
   temp <- data %>%
     subset(select = 1)
   
-    
+  # Find the index of each row containing the string 'Site: ".  
   temp.ind <- which(
     str_detect(
       temp$X1,
       "Site: "
       )
     )
-    
-    temp.ind.diff <- diff(
-      temp.ind,
-      lag=1
-      )
-    
-    temp.val <- slice(
-      temp,
-      temp.ind
-      )
-    
-    if(length(temp)<temp.ind[length(temp.ind)]){
-      temp.ind.diff[length(temp.ind.diff)+1] <- nrow(temp)-temp.ind[length(temp.ind)] + 1
-    } else {
-      temp.ind.diff[length(temp.ind.diff)+1] <- nrow(temp)-temp.ind[length(temp.ind)- 1] + 1  
-    }
   
+  # Find the difference between each index and save into a 
+  # vector of differences.
+  temp.ind.diff <- diff(
+    temp.ind,
+    lag=1
+    )
+  
+  # Return a vector of only the values corres. to the indices
+  # found by temp.ind
+  temp.val <- slice(
+    temp,
+    temp.ind
+    )
+  
+  # The heart of this statement is used to adjust for the fact that
+  # the vector of differences is too short (-1). This happens because
+  # the diff() function doesn't return the difference between the last
+  # value and the end of the data set - only the differences between values.
+  
+  # The if accounts for an error that occurs when the last index = the number of
+  # rows in the data.
+  if(length(temp)<temp.ind[length(temp.ind)]){
+    temp.ind.diff[length(temp.ind.diff)+1] <- nrow(temp)-temp.ind[length(temp.ind)] + 1
+  } else {
+    temp.ind.diff[length(temp.ind.diff)+1] <- nrow(temp)-temp.ind[length(temp.ind)- 1] + 1  
+  }
+  
+  # This merges the original data with another column of "Sites: ....", replicated
+  # by the numbers in the diff vector.
   clean.data2 <- cbind(
     temp,
     rep(
@@ -120,14 +139,29 @@ import.data <-
       )
     )
   
+  # This removes the "Site: ..." rows from the original column vector, thus 
+  # finally separating the site data into a separate (column) variable from the 
+  # rest of the data.
   clean.data2 <- clean.data2[-c(temp.ind),]
-
-  rm(data, temp, temp.val, temp.ind, temp.ind.diff)
   
+  # The following line of code can help resolve indivual issues
+  # you may encounter with the data: in this example, a client's
+  # name has been appended to their MRN. The code replaces the 
+  # Combined field with their MRN only.
+  
+  #clean.data2[,1] <- gsub("999999: Mr Benjamin Moran", "999999", clean.data2[,1])
+  clean.data2[,1] <- gsub("6048086: Mr Golin Nepia", "6048086", clean.data2[,1])
+  data <- clean.data2
+  rm(clean.data2, temp, temp.val, temp.ind, temp.ind.diff)
+  
+
 ## Filter Cost Centres ================================= 
 
+  # The principles here are the same as the previous section, just accounting
+  # for a different variable (Cost Centres).
+  
   # Use the output from the previous section.
-  data <- clean.data2
+
     
   temp <- data %>%
     subset(select = 1)
@@ -166,25 +200,106 @@ import.data <-
   )
   clean.data3 <- clean.data3[-c(temp.ind),]
   
+  # The data contains information on the dispensing setting that is not
+  # necessary for our purposes. The next few lines removes these rows from
+  # the data.
+  
+  clean.data3 <- clean.data3[!str_detect(clean.data3[,1],"Inpatient"),]
+  clean.data3 <- clean.data3[!str_detect(clean.data3[,1],"Outpatient"),]
+  clean.data3 <- clean.data3[!str_detect(clean.data3[,1],"Discharge"),]
+  
   rm(data, temp, temp.val, temp.ind, temp.ind.diff)
-
-## Up to here!!!!!!!! ============
-
-# NOTE: NEED TO CLEAR OUT INPATIENT, OUTPATIENT & DISCHARGE FROM COL.
-break() 
+  
+## Split MRNs and Drugs ==================================
+  
+  # This step will end with all variables separated into individual columns, 
+  # with each row representing a single observation/record.
+  
   data <- clean.data3
   
   temp <- data %>%
     subset(select = 1)
+  temp2 <- data %>%
+    subset(select = -1)
   
   temp.mrn <- temp[ c(TRUE,FALSE),]
   temp.drg <- temp[ !c(TRUE,FALSE),]
-  temp.comb <- cbind(temp.mrn,temp.drg)
-  
-  data.a <- cbind(temp.comb, clean.data[,2:ncol(clean.data)])
+  temp.other <- temp2[ c(TRUE,FALSE),]
+  data.a <- cbind(temp.mrn,temp.drg,temp.other)
+
   names(data.a) <- c("MRN","Drug_Disp","Site","Cost_Centre")
 
-  rm(clean.data, data, import.data, temp, temp.comb, temp.mrn, temp.drg)
-## Concatenate both datasets
+  rm(clean.data3, data, import.data, temp, temp2, temp.mrn, temp.drg, temp.other)
+
+  ## Concatenate both datasets
   
   final <- cbind(data.a, data.b)
+  
+  rm(data.a, data.b)
+  
+
+## Clean Up MRNs ==============================================================  
+  # we need to do some minor housekeeping on the data.
+  
+  # Converts (letter) O into (number) 0. Sometimes MRNs come with  leading 
+  # zeroes replaced to prevent Excel trimming them. We don't want them.
+  final$MRN<- gsub("O", 0, final$MRN)
+  
+  # Removes # marks placed at the front of some of the MRNs.
+  final$MRN <- gsub("#", "", final$MRN)
+  
+  # Removes unnescessary text from the Site variable.
+  final$Site <- gsub("Site: ","", final$Site)
+  
+  # Rounds the Cost variable to 2 decimal places so that it reads accurately as
+  # dollars and cents.
+  final$Cost <- round(final$Cost,2)
+  
+  # Reformats Date variable from character to date.
+  final$Date <- dmy(final$Date)
+  
+  # Some of the MRNs are recognised as numbers and R formats them accordingly,
+  # i.e. with decimal places. After some other conversions they become 
+  # characters again, so we need to trim the decimals.
+  
+  for(i in 1:nrow(final)){
+    if(str_detect(final[i,1],".0")==TRUE){
+      final[i,1] = str_split_fixed(final[i,1],".0",2)[1]
+    }
+  }
+  
+  # Remove unnescessary variables.
+  rm(i)
+  
+
+## Export Data =================================================================
+  
+  # This code exports the data to .csv files in the working directory.
+  
+  # Where the files will be saved
+  getwd()
+  
+  # All Dispensing
+  final.Dispensing <- final %>%
+    filter(., Cost>=0)
+  
+  # All Reversals
+  final.Reversals <- final %>%
+    filter(.,Cost<0)
+  
+  write.csv(
+    final,
+    "final.csv",
+    row.names = FALSE
+    )
+  write.csv(
+    final.Dispensing,
+    "finalDispensing.csv",
+    row.names = FALSE
+    )
+  write.csv(
+    final.Reversals,
+    "finalReversals.csv",
+    row.names = FALSE
+    )
+  rm()
